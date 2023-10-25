f5a520b97fa7c1168751ecced0abbcbbc03739ab
huwenhao <wumanho@tutanota.com>
2023-10-25 18:04:22 +0800

    chore：函数名和注释修改

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -5,7 +5,7 @@ type ProxyCallback = (args: unknown[]) => unknown
 export function createClientProxy<R extends AppRouter>(url: string): R {
   const proxy = new Proxy({} as R, {
     get(_, key) {
-      return createRecursiveProxy(async (args) => {
+      return createInnerProxy(async (args) => {
         const result = await fetch(url, {
           method: 'POST',
           headers: {
@@ -29,15 +29,14 @@ export function createClientProxy<R extends AppRouter>(url: string): R {
 }
 
 /**
- * 递归创建代理，目的是拿到函数的参数
+ * 创建一个新的代理对象返回给调用方
+ * 返回值是一个函数，调用该函数会触发代理对象的 apply 方法
+ * 获取到传入的参数后，调用 callback
  * @param callback
  * @returns
  */
-function createRecursiveProxy(callback: ProxyCallback) {
-  return new Proxy(() => {}, {
-    get() {
-      return createRecursiveProxy(callback)
-    },
+function createInnerProxy(callback: ProxyCallback) {
+  return new Proxy(callback, {
     apply(_1, _2, args) {
       return callback(args)
     }
